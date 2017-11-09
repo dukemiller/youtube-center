957908bd2c57edf844b68b771e8da586651db12f
dukemiller <dukemiller928@hotmail.com>
2017-11-09 09:09:40 -0700

    Make chanel model a reactive object

--- youtube-center/Models/Channel.cs
+++ youtube-center/Models/Channel.cs
@@ -1,20 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using GalaSoft.MvvmLight;
 using Newtonsoft.Json;
-using youtube_center.Repositories.Interface;
+using youtube_center.Repositories;
 
 namespace youtube_center.Models
 {
     [Serializable]
-    public class Channel
+    public class Channel: ObservableObject
     {
+        private string _id = string.Empty;
+        private string _name = string.Empty;
+        private string _url = string.Empty;
+
+        // 
+
         [JsonProperty("id")]
-        public string Id { get; set; }
+        public string Id
+        {
+            get => _id;
+            set => Set(() => Id, ref _id, value);
+        }
 
         [JsonProperty("name")]
-        public string Name { get; set; }
-        
+        public string Name
+        {
+            get => _name;
+            set => Set(() => Name, ref _name, value);
+        }
+
+        [JsonProperty("url")]
+        public string Url
+        {
+            get => _url;
+            set => Set(() => Url, ref _url, value);
+        }
+
         [JsonProperty("videos")]
         public List<Video> Videos { get; set; } = new List<Video>();
 
