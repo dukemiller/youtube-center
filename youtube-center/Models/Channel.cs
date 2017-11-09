9e5fa8c9b3fc845312b249215347ec6387bd3f66
dukemiller <dukemiller928@hotmail.com>
2017-11-09 06:38:49 -0700

    Add serialization attributes to channel model

--- youtube-center/Models/Channel.cs
+++ youtube-center/Models/Channel.cs
@@ -1,11 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using youtube_center.Repositories.Interface;
 
 namespace youtube_center.Models
 {
+    [Serializable]
     public class Channel
     {
-        public string Name { get; set; }
+        [JsonProperty("id")]
         public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("videos")]
         public List<Video> Videos { get; set; } = new List<Video>();
     }
 }
