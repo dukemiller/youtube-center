b7f7be52e0e7ba4cacb64ac7b3e57acf690aa0f3
dukemiller <dukemiller928@hotmail.com>
2017-11-09 05:07:18 -0700

    Add basic viewmodellocator

---
+++ youtube-center/ViewModelLocator.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using youtube_center.Repositories.Interface;
+using youtube_center.Services;
+using youtube_center.Services.Interface;
+using youtube_center.ViewModels;
+
+namespace youtube_center
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            // Repositories
+            // SimpleIoc.Default.Register<ISettingsRepository>(SettingsRepository.Load);
+
+            // Services
+            SimpleIoc.Default.Register<IYoutubeService, YoutubeService>();
+
+            // Viewmodels
+            SimpleIoc.Default.Register<MainWindowViewModel>();
+        }
+
+        public static MainWindowViewModel Main => ServiceLocator.Current.GetInstance<MainWindowViewModel>();
+
+    }
+}
