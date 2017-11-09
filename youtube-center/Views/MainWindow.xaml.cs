cecfbf0383edd96d93f4e2355a115b01368b0ce0
dukemiller <dukemiller928@hotmail.com>
2017-11-09 06:11:11 -0700

    Nuget: MahApps, style mainwindow view

--- youtube-center/Views/MainWindow.xaml.cs
+++ youtube-center/Views/MainWindow.xaml.cs
@@ -5,7 +5,7 @@ namespace youtube_center
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
