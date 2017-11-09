97ab1920ab1e1513a10b140fdc2225164da83b3d
dukemiller <dukemiller928@hotmail.com>
2017-11-09 06:27:36 -0700

    Load videos from settings

--- youtube-center/ViewModels/MainWindowViewModel.cs
+++ youtube-center/ViewModels/MainWindowViewModel.cs
@@ -5,21 +5,24 @@ using System.Linq;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using youtube_center.Models;
+using youtube_center.Repositories.Interface;
 using youtube_center.Services.Interface;
 
 namespace youtube_center.ViewModels
 {
     public class MainWindowViewModel: ViewModelBase
     {
+        private readonly ISettingsRepository _settingsRepository;
         private readonly IYoutubeService _youtubeService;
         private ObservableCollection<Video> _videos = new ObservableCollection<Video>();
         private Video _selectedVideo;
 
-        public MainWindowViewModel(IYoutubeService youtubeService)
+        public MainWindowViewModel(ISettingsRepository settingsRepository, IYoutubeService youtubeService)
         {
+            _settingsRepository = settingsRepository;
             _youtubeService = youtubeService;
 
-            // Load videos from _settings later
+            Videos = new ObservableCollection<Video>(_settingsRepository.Channels.SelectMany(channel => channel.Videos).OrderByDescending(video => video.Uploaded).ThenBy(video => video.Title));
 
             TestCommand = new RelayCommand(Test);
         }
@@ -42,10 +45,16 @@ namespace youtube_center.ViewModels
         {
             // https://www.youtube.com/subscription_manager
             // https://www.youtube.com/feeds/videos.xml?channel_id=UCtUbO6rBht0daVIOGML3c8w
+            // TODO check if theres a better way to get every channel later?
 
-            var testChannel = new Channel {Id = "UCtUbO6rBht0daVIOGML3c8w"};
-            var videos = new List<Video>(await _youtubeService.RetrieveVideos(testChannel));
-            Videos = new ObservableCollection<Video>(videos.OrderByDescending(video => video.Uploaded).ThenBy(video => video.Title));
+            foreach (var channel in _settingsRepository.Channels)
+            {
+                channel.Videos = new List<Video>(await _youtubeService.RetrieveVideos(channel));
+            }
+            _settingsRepository.LastChecked = DateTime.Now;
+            _settingsRepository.Save();
+
+            Videos = new ObservableCollection<Video>(_settingsRepository.Channels.SelectMany(channel => channel.Videos).OrderByDescending(video => video.Uploaded).ThenBy(video => video.Title));
         }
 
     }
