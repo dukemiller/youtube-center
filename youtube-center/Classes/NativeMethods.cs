caf6e74679fbe9599303aa216869d127cfa82188
dukemiller <dukemiller928@hotmail.com>
2017-11-10 06:31:44 -0700

    Only open the process a single time, refocus on attempting to open

---
+++ youtube-center/Classes/NativeMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace youtube_center.Classes
+{
+    public static class NativeMethods
+    {
+        private const int Restore = 9;
+
+        /// <summary>
+        ///     Focus the opened downloader.
+        /// </summary>
+        public static void FocusOther()
+        {
+            var hwnd = FindWindow(null, "Youtube Center");
+            ShowWindow(hwnd, Restore);
+            SetForegroundWindow(hwnd);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr FindWindow(string sClassName, string sAppName);
+    }
+}
--- youtube-center/Views/MainWindow.xaml.cs
+++ youtube-center/Views/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Windows;
 using GalaSoft.MvvmLight.Ioc;
 using MahApps.Metro.Controls.Dialogs;
+using youtube_center.Classes;
 
 namespace youtube_center.Views
 {
@@ -11,7 +15,26 @@ namespace youtube_center.Views
     {
         public MainWindow()
         {
-            InitializeComponent();
+            if (AlreadyOpen)
+            {
+                NativeMethods.FocusOther();
+                Close();
+            }
+
+            else
+                InitializeComponent();
+        }
+
+        /// <summary>
+        ///     Returns the check if there is an already opened anime downloader.
+        /// </summary>
+        private static bool AlreadyOpen
+        {
+            get
+            {
+                var name = Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location);
+                return Process.GetProcessesByName(name).Length > 1;
+            }
         }
 
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
